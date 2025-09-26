001b6b0d14f94fc5b4eb51656d4358327d231666
Hitesh <choudharyhitesh905@gmail.com>
2025-09-26 19:37:39 +0530

    targeting the html id to perform DOM functionalites

---
+++ script.js
@@ -0,0 +1,3 @@
+const inputBox=document.getElementById('todo-input')//the text field
+const addtask=document.getElementById('add-task-btn')//the button which add a task to a list
+const tasks=document.getElementById('todo-list')//the unordered list container
